f51bffd4811c54465a3e1ce26caaa9599168bbee
Ali-consciousroad <ali_dindar@live.be>
2021-09-12 16:28:01 +0200

    Adding JavaScript logic to our components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,9 +6,18 @@ import './ExpenseItem.css';
 */
 function ExpenseItem(props) {
   // By using props we can now take our data from outise our ExenseItem.js component 
+  // Good practice: To keep our code clean, we take out the logic from the JSX code
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
