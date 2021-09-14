c90edeea3258e6318ae1920329d7c246bf25cc64
Ali-consciousroad <ali_dindar@live.be>
2021-09-14 10:46:01 +0200

    Working with state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
+// Component logic
 // Our first custom component
 /* In React we won't use many parameters like in regular JS, we'll use only one parameter named usually props
    that will store all our props/
    props will receive key value pair to precises.
 */
 const ExpenseItem = (props) => {
-
-  // Component logic
+  // We use destructuring to store the 2 values inside the array returned by the useState() hook
+  // Always 2 values: A constant an a state updating function 
+  const [title, setTitle] = useState(props.title); // Can be used inside React function
   
   /* Not a must do, but useful to add handler in the name of our function 
      so we know that this function react to an event and is not used elsewhere in our code */
-  
   // Here we call the clickHandler function 
   // function clickHandler()   // Old JS syntax
   const clickHandler = () => {
-    console.log('Clicked!!!');
+    /* This will not only store a new value but reavalute again the component this special function is in 
+    when the state changes */
+    setTitle('Updated!');  
+    // title = "Updated"; // This will not work
+    console.log(title); 
   };
 
   // Component view
@@ -30,7 +35,7 @@ const ExpenseItem = (props) => {
       we need to add an attribute to the imported <ExpenseDate /> component we use here */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
       </div>
       <button onClick={(clickHandler)}>Change Title</button>
