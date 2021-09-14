d4d97f8ea5055dab9670cae843dfed8cf0270183
Ali-consciousroad <ali_dindar@live.be>
2021-09-14 13:00:33 +0200

    Listening to user input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { createPortal } from "react-dom";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    // Vanilla JS
+    // We automatically get an object event by listening to events  
+    // document.getElementById('').addEventListener('click', (event) => {})
+    // The same is true here with React
+    const titleChangeHandler = (event) => {
+        // console.log(event);
+        // Names found in the console: This will display the input value
+        console.log(event.target.value);
+    };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
