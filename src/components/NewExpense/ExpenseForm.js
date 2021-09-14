9c37d2ed854b8608e5d1f4eb4f09e2eedcc4cb6e
Ali-consciousroad <ali_dindar@live.be>
2021-09-14 16:18:21 +0200

    Syntax for updating state that depends on the previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,54 +4,59 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   // We can have multiple state in a component
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   /* Instead of using multiple states, 
     we use here one state with one object */
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
   // Vanilla JS
   // We automatically get an object event by listening to events
   // document.getElementById('').addEventListener('click', (event) => {})
   // The same is true here with React
   const titleChangeHandler = (event) => {
-       console.log(event);
+    console.log(event);
     // Names found in the console: This will display the input value
-    // console.log(event.target.value);
+    console.log(event.target.value);
 
     // Store the value in our state
     // Used with multiple states
     // setEnteredTitle(event.target.value);
 
     // New way with one state
-    setUserInput({
-      // We need to use the spread operator to keep track of the state of our other key pairs
-      // otherwise their value will be lost
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    // setUserInput({
+    //   // We need to use the spread operator to keep track of the state of our other key pairs
+    //   // otherwise their value will be lost
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    /* Whenever working with previous state, 
+    here this is the right way to be sure we are working with the latest state snapshot. */
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
   const amountChangeHandler = (event) => {
     console.log(event.target.value);
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
     console.log(event.target.value);
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
