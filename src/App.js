592f0044dcadc3679c64b1a5131a5c0679e12e0f
Ali-consciousroad <ali_dindar@live.be>
2021-09-15 20:19:25 +0200

    Using stateful lists

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // We can find the code transformed in JS in the chrome browser console 
 // main.chunk.js (from line 43)
 
@@ -7,11 +7,10 @@ import React from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
 
-
     // 4 objects added in our expenses array
     // expenses const given (udemy resource folder)
-  const App = () => {
-    const expenses = [
+
+    const DUMMY_EXPENSES = [
       {
         id: 'e1',
         title: 'New Desk Chair',
@@ -33,10 +32,18 @@ import NewExpense from './components/NewExpense/NewExpense';
       },
     ];
 
-    const addExpenseHandler = expense => {
-      console.log('In App.js');
-      console.log(expense);
-    };  
+    const App = () => {
+      const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+      const addExpenseHandler = expense => {
+        // Not the clean way 
+        // setExpenses([expense, ...expenses]);
+        // Clean way to update the state based on a older snapshot of the same state
+        setExpenses((prevExpenses) => {
+          return [expense, ...prevExpenses];
+        });
+      };
+
 
   // Show what JSX does behind the scene
   // If we use React like this, we need to import React
