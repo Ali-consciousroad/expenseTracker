813490238af97e18dafd942d57c8c94d64f6adf2
Ali-consciousroad <ali_dindar@live.be>
2021-09-16 16:22:41 +0200

    Adding conditional return statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
@@ -12,18 +12,20 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   // Component view
   return (
-    <Card className="expense-item">
-      {/*<ExpenseDate></ExpenseDate>*/}
-      {/* The component can be written like a self-closing tag if nothing inside it */}
-      {/* Now that we have created a new <ExpenseDate /> component, 
+    <li>
+      <Card className="expense-item">
+        {/*<ExpenseDate></ExpenseDate>*/}
+        {/* The component can be written like a self-closing tag if nothing inside it */}
+        {/* Now that we have created a new <ExpenseDate /> component, 
       we need to add an attribute to the imported <ExpenseDate /> component we use here */}
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}€</div>
-      </div>
-    </Card>
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}€</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
