40185267b2387c7e640256342df3ecfb1f6195df
Ali-consciousroad <ali_dindar@live.be>
2021-09-16 18:34:00 +0200

    Correction - Assignement 4: Conditional content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import React, { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
-      ...enteredExpenseData, 
-      // Not a perfect id, we could generate the same id twice 
-      id: Math.random().toString()
+      ...enteredExpenseData,
+      // Not a perfect id, we could generate the same id twice
+      id: Math.random().toString(),
     };
     // console.log(expenseData);
     props.onAddExpense(expenseData);
-  }
+    // Also close the form after being submitted 
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
 
   return (
     <div className="new-expense">
@@ -19,7 +31,17 @@ const NewExpense = (props) => {
       We follow this convention just to make it clear 
       this props value should be a function */}
       {/* Don't call but point to the newly created function : saveExpenseDataHandler() */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {/* Show the buttong if isEditing is false */}
+      {!isEditing && 
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      }
+      {/* Show the form if isEditing is true */}
+      {isEditing && 
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      }
     </div>
   );
 };
